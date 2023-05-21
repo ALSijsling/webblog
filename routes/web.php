a13b053dbfae7c066e33571485f1733a70648870
Lana Sijsling <125027260+ALSijsling@users.noreply.github.com>
2023-05-21 18:16:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::resources([
     'posts' => PostController::class,
     'users' => UserController::class
